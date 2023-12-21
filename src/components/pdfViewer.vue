34707079a1e4f8f5d20f2aa98612e03dd5da3890
omarjawedi <omarjawedic2i@gmail.com>
2023-12-22 00:52:19 +0100

    fix

--- src/components/pdfViewer.vue
+++ src/components/pdfViewer.vue
@@ -1,154 +1,200 @@
 <template>
-	<div>
-		<h1>My PDF Viewer</h1>
-
+	<div class="flex flex-col border relative max-h-full" ref="pdfViewer">
+	
+		<PdfApp ref="pdfApp" :pdf="pdfLink" :config="config3" :idConfig="idConfig"  @open="openHandler"  >
+	
+		</PdfApp>
+		<div class="flex flex-row w-full justify-around bg-cancel py-1 viewer-footer">
+			<div class=""></div>
 			<div class="flex flex-row space-x-4 items-center">
-				<span>{{ pageNumber.toString() }} /{{ pageCount }}</span>
-				<button :id="idConfig.zoomOut" type="button" @click="zoomOut">Zoom out</button>
-				<button :id="idConfig.zoomIn" type="button" @click="zoumIn">Zoom in</button>
+				<div>
+					<input type="number" class="inputPage" :id="idConfig.pageNumber" > / <span> {{pageCount}}</span>
+				</div>
+				<button :id="idConfig.zoomOut" type="button" @click="zoomOut"><span class="mdi mdi-minus-circle-outline">zoom out</span></button>
+				<button :id="idConfig.zoomIn" type="button" @click="zoomIn">Zoom in</button>
 				<button :id="idConfig.presentationMode" type="button">toggleFullscreen</button>
-				<button :id="idConfig.next" type="button" @click="nextPage" >nextpage</button>
-				<button :id="idConfig.previous" type="button" @click="prevPage"> previousPage</button>
+				<button :id="idConfig.nextPage" type="button" >nextpage</button>
+				<button :id="idConfig.previousPage" type="button" > previousPage</button>
+				<button  @click="toggleFullscreen">{{isFullscreen ? 'fullscreen' : 'exit'}}</button>
 			</div>
-			<PdfApp ref="pdfApp" :pdf="pdfLink" :config="config3" :idConfig="idConfig"  @open="openHandler"  />
-
-		
+		</div>		
 	</div>
-  </template>
-<script>
-
-import "vue-pdf-app/dist/icons/main.css";
-import PdfApp from "vue-pdf-app";
-export default {
-components: {
-    PdfApp,
-  },
-  data(){
-    return{
-        pdfUrl:"https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf",
-		idConfig: { 
-			zoomIn: "zoomInId", 
-			zoomOut: "zoomOutId",
-			presentationMode: "presentationModeId",
-			next:"next",
-			previous:"previous",
-			pageNumber:"pageNumber"
+	</template>
+	<script>
+	
+	import "vue-pdf-app/dist/icons/main.css";
+	import PdfApp from "vue-pdf-app";
+	export default {
+	components: {
+		PdfApp,
+	  },
+	  data(){
+		return{
+			pdfUrl:"https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf",
+			idConfig: { 
+				zoomIn: "zoomInId", 
+				zoomOut: "zoomOutId",
+				presentationMode: "presentationModeId",
+				nextPage: "nextPageId",
+				previousPage: "previousPageId",
+				pageNumber: "pageNumberId"
+			},
+			info: [] ,
+			config3: {
+				toolbar: false,
+				sidebar:false
+			},
+			pageCount: 0,
+			page:1,
+			isFullscreen: false,
+			}
+	
+	  },
+	  computed: {
+		pdfLink() {
+		  return "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf";
 		},
-        // config: {
-		// 	sidebar: {
-		// 	viewThumbnail: false,
-		// 	viewOutline: false,
-		// 	viewAttachments: false,
-		// 	},
-		// 	findbar: false,
-		// 	secondaryToolbar: {
-		// 	secondaryPresentationMode: true,
-		// 	secondaryOpenFile: true,
-		// 	secondaryPrint: true,
-		// 	secondaryDownload: true,
-		// 	secondaryViewBookmark: true,
-		// 	firstPage: true,
-		// 	lastPage: true,
-		// 	pageRotateCw: true,
-		// 	pageRotateCcw: true,
-		// 	cursorSelectTool: true,
-		// 	cursorHandTool: true,
-		// 	scrollVertical: true,
-		// 	scrollHorizontal: true,
-		// 	scrollWrapped: true,
-		// 	spreadNone: true,
-		// 	spreadOdd: true,
-		// 	spreadEven: true,
-		// 	documentProperties: true,
-		// 	},
-		// 	toolbar: {
-		// 	toolbarViewerLeft: {
-		// 		previous: true,
-		// 		next: true,
-		// 		pageNumber: true,
-				
-		// 	},
-		// 	toolbarViewerRight: {
-		// 		presentationMode: true,
-		// 		openFile: true,
-		// 		print: true,
-		// 		download: true,
-		// 		viewBookmark: true,
-		// 		secondaryToolbarToggle: true,
-		// 	},
-		// 	toolbarViewerMiddle: {
-		// 		zoomOut: true,
-		// 		zoomIn: true,
-		// 		scaleSelectContatiner: true,
-		// 	},
-		// 	},
-		// 	viewerContextMenu: false,
-		// 	errorWrapper: true,
-     	//  }, 
-		info: [] ,
-		config3: {
-			toolbar: false,
-			sidebar:false
-		},
-		pageCount: 0,
-		page:1,
-		isFullscreen: false,
+		pageNumber() {
+			return this.page
 		}
-
-  },
-  computed: {
-    pdfLink() {
-      return "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf";
-    },
-	pageNumber() {
-		return this.page
+	  },
+	  methods: {
+		async openHandler(pdfApp) {
+		  this.info = [];
+		  console.log(pdfApp.pagesCount)
+		  this.page= pdfApp.page
+		  this.pageCount = pdfApp.pagesCount
+		  console.log(pdfApp.page)
+		  const info = await pdfApp.pdfDocument
+			.getMetadata()
+			.catch(console.error.bind(console));
+	
+		  if (!info) return;
+		  const props = Object.keys(info.info);
+		  props.forEach((prop) => {
+			const obj = {
+			  name: prop,
+			  value: info.info[prop]
+			};
+			this.info.push(obj);
+		  });
+		},
+		nextPage() {
+		  if (this.page < this.pageCount) {
+			this.page += 1;
+			console.log('this page number is', this.page)
+		  }
+		},
+		prevPage() {
+		  if (this.page > 1) {
+			this.page -= 1;
+			console.log('this page number is', this.page)
+		  }
+		},
+		makeFullscreen() {
+		  const fullscreenContainer = this.$refs.pdfApp;
+			console.log('fullscreen', fullscreenContainer)
+		  // Check if the browser supports the Fullscreen API
+		  if (fullscreenContainer.requestFullscreen) {
+			fullscreenContainer.requestFullscreen();
+		  } else if (fullscreenContainer.mozRequestFullScreen) {
+			// Firefox
+			fullscreenContainer.mozRequestFullScreen();
+		  } else if (fullscreenContainer.webkitRequestFullscreen) {
+			// Chrome, Safari, and Opera
+			fullscreenContainer.webkitRequestFullscreen();
+		  } else if (fullscreenContainer.msRequestFullscreen) {
+			// IE/Edge
+			fullscreenContainer.msRequestFullscreen();
+		  }
+	
+		  // Update the isFullscreen variable
+		  this.isFullscreen = true;
+		},
+		exitFullscreen() {
+		  // Check if the browser supports the Fullscreen API
+		  if (document.exitFullscreen) {
+			document.exitFullscreen();
+		  } else if (document.mozCancelFullScreen) {
+			// Firefox
+			document.mozCancelFullScreen();
+		  } else if (document.webkitExitFullscreen) {
+			// Chrome, Safari, and Opera
+			document.webkitExitFullscreen();
+		  } else if (document.msExitFullscreen) {
+			// IE/Edge
+			document.msExitFullscreen();
+		  }
+	
+		  // Update the isFullscreen variable
+		  this.isFullscreen = false;
+		},
+		toggleFullscreen() {
+		  if (this.isFullscreen) {
+			this.exitFullscreen();
+		  } else {
+			this.makeFullscreen();
+		  }
+		},
+		handleFullscreenChange() {
+		  // Update the isFullscreen variable when the fullscreen state changes
+		  this.isFullscreen = !!(
+			document.fullscreenElement ||
+			document.mozFullScreenElement ||
+			document.webkitFullscreenElement ||
+			document.msFullscreenElement
+		  );
+		  
+		},
+	  },
+	  mounted() {
+		// Listen for the fullscreenchange event
+		document.addEventListener("fullscreenchange", this.handleFullscreenChange);
+		document.addEventListener(
+		  "mozfullscreenchange",
+		  this.handleFullscreenChange
+		);
+		document.addEventListener(
+		  "webkitfullscreenchange",
+		  this.handleFullscreenChange
+		);
+		document.addEventListener(
+		  "msfullscreenchange",
+		  this.handleFullscreenChange
+		);
+	  },
+	  beforeDestroy() {
+		// Remove the event listeners when the component is destroyed
+		document.removeEventListener(
+		  "fullscreenchange",
+		  this.handleFullscreenChange
+		);
+		document.removeEventListener(
+		  "mozfullscreenchange",
+		  this.handleFullscreenChange
+		);
+		document.removeEventListener(
+		  "webkitfullscreenchange",
+		  this.handleFullscreenChange
+		);
+		document.removeEventListener(
+		  "msfullscreenchange",
+		  this.handleFullscreenChange
+		);
+		
+	  },
+	};
+	</script>
+	<style >
+	.inputPage{
+		max-width: 50px;
 	}
-  },
-  methods: {
-    async openHandler(pdfApp) {
-      this.info = [];
-	  console.log(pdfApp.pagesCount)
-	  this.page= pdfApp.page
-	  this.pageCount = pdfApp.pagesCount
-	  console.log(pdfApp.page)
-      const info = await pdfApp.pdfDocument
-        .getMetadata()
-        .catch(console.error.bind(console));
-
-      if (!info) return;
-      const props = Object.keys(info.info);
-      props.forEach((prop) => {
-        const obj = {
-          name: prop,
-          value: info.info[prop]
-        };
-        this.info.push(obj);
-      });
-    },
-	test() {
-		console.log(this.$refs.pdfApp)
-	},
-	zoomOut() {
-      console.log('zoom out')
-    },
-    zoomIn() {
-		console.log('zoom in')
-    },
-    toggleFullscreen() {
-		console.log('fullscreen')
-    },
-    nextPage() {
-      if (this.page < this.pageCount) {
-        this.page += 1;
-		console.log('this page number is', this.page)
-      }
-    },
-    prevPage() {
-      if (this.page > 1) {
-        this.page -= 1;
-		console.log('this page number is', this.page)
-      }
-    },
-  }
-};
-</script>
+	.viewer-footer {
+		position: absolute;
+		bottom: 0px;
+		right: 0;
+		left: 0;
+		background-color: antiquewhite;
+	  }
+	
+	</style>
